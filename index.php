fd9277f4e333f421a4e4fdbbd6d0a7edb65bc236
JosuegadirSierraAlfonso <josuegsierraa18@gmail.com>
2023-06-01 17:38:40 -0500

    feat: :tada: php
    Init

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+require_once("/var/www/html/SkylAb-175/CRUD-PHP/view/head/head.php");
+?>
+
+<a href="../CRUD-PHP/view/userName/create.php" class="btn btn-primary">Add new user</a>
+
+<?php
+require_once("/var/www/html/SkylAb-175/CRUD-PHP/view/head/footer.php")
+
+?>
